088b973f5d7ff9938e9f8953046f425a657dbb2e
Sreenath Mudigonda <>
2024-11-29 11:27:00 +0530

    empty login tests

---
+++ my-demo-app-ios-main/MyDemoAppUITests/E2ETests/Login/LoginWithEmptyFieldsTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoginWithEmptyFieldsTests.swift
+//  MyDemoAppUITests
+//
+//  Created by Sreenath Mudigonda on 29/11/24.
+//
+
+import XCTest
+
+final class LoginWithEmptyFieldsTests: BaseTest {
+
+    var homeScreen : HomeScreen!
+    var moreScreen : MoreScreen!
+    var loginScreen : LoginScreen!
+  //  var userCredentialsTestData: UserCredentialsTestDataModel!
+    
+    override func setUp() {
+        super.setUp()
+        loadScreens()
+       //loadTestData()
+    }
+    
+    func loadScreens(){
+        
+    }
+    
+    func testEmptyLogin(){
+        
+    }
+    
+    
+}
